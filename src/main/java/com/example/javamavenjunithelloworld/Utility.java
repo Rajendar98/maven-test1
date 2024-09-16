98bafa5320b2f593c0e3049f9f5073a69117b76f
Rajendar <srimarthirajendar2002@gmail.com>
2024-09-16 18:49:03 +0530

    RajendarR0001 - Added utility function to check divisibility by 2

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,10 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    public static boolean isDivisibleBy2(int number) {
+        return number % 2 == 0;
+    }
+    public static void rajendar() {
+        System.out.println("I am Rajendar");
+    }
+}
